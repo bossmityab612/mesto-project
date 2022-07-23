f565145acfa65590bf0f7c524a2aa4d6e157b667
Anastasia Bashkirova <bossmityab612@yandex.ru>
2022-07-23 23:51:55 +0300

    style: add-button edit

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-let popupOpened = popup.querySelector('.popup__container');
+const popupOpened = document.querySelector('.profile__description_button_edit');
+const popup = document.querySelector('.popup');
+const popupClosed = 
 
+function popupOpened() {
+    
+}
+
+
+
+  
