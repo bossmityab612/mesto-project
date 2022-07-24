e00bd27667c6b651af26dbec995c24aa12561825
Anastasia Bashkirova <bossmityab612@yandex.ru>
2022-07-24 13:12:06 +0300

    js: make a fbutton

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-const popupOpened = document.querySelector('.profile__description_button_edit');
+const openPopupButton = document.querySelector('.profile__description_button_edit');
 const popup = document.querySelector('.popup');
-const popupClosed = 
+const closePopupButton = popup.querySelector('.popup__container_button_exit');
+const popupButtonSubmit = popup.querySelector('.popup__add-button');
 
-function popupOpened() {
-    
+// Открытие попапа
+function popupOpenToggle() {
+    popup.classList.toggle('popup_opened');
+}
+
+function openPopup() {
+    const popupName = popup.querySelector('.popup__form_name');
+    const popupJob = popup.querySelector('.popup__form_about');
+    const editProfileTitle = document.querySelector('.profile__description-text_name');
+    const editProfileSubtitle = document.querySelector('.profile__description-text_career');
+    //определяем переменную для редактирования строк в попапе
+    //присваивание value к тому, что будем вводить в строку
+    popupName.value = editProfileTitle.textContent;
+    popupJob.value = editProfileSubtitle.textContent;
+    popup.classList.add('popup_opened');
 }
 
 
-
-  
+// Закрытие попапа
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
