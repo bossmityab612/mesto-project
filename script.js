6d69b86ff33bb82c9b408dcd47858707523f0d78
Anastasia Bashkirova <bossmityab612@yandex.ru>
2022-08-18 16:35:32 +0300

    style: fin popup__opened

--- script.js
+++ script.js
@@ -1,27 +1,17 @@
-const openPopupButton = document.querySelector('.profile__description_button_edit');
-const popup = document.querySelector('.popup');
-const closePopupButton = popup.querySelector('.popup__container_button_exit');
-const popupButtonSubmit = popup.querySelector('.popup__add-button');
+const popupOpened = document.querySelector('.popup'); //Всплывающее окно
+const popup = document.querySelector('.popup__container');
+const openPopupButton = document.querySelectorAll('.profile__description_button_edit'); //Кнопка открытия попапа
+const closePopupButton = document.querySelector('.popup__container_button_exit'); //Кнопка закрытия попапа
 
-// Открытие попапа
-function popupOpenToggle() {
-    popup.classList.toggle('popup__opened');
-}
+openPopupButton.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        popupOpened.classList.add('popup__opened');
+        popup.classList.add('popup__opened');
+    })
+});
 
-function openPopup() {
-    const popupName = popup.querySelector('.popup__form_name');
-    const popupJob = popup.querySelector('.popup__form_about');
-    const editProfileTitle = document.querySelector('.profile__description-text_name');
-    const editProfileSubtitle = document.querySelector('.profile__description-text_career');
-    //определяем переменную для редактирования строк в попапе
-    //присваивание value к тому, что будем вводить в строку
-    popupName.value = editProfileTitle.textContent;
-    popupJob.value = editProfileSubtitle.textContent;
-    popup.classList.add('popup__opened');
-}
-
-
-// Закрытие попапа
-function closePopup() {
-    popup.classList.remove('popup__opened');
-}
+closePopupButton.addEventListener('click',() => {
+        popupOpened.classList.remove('popup__opened');
+        popup.classList.remove('popup__opened');
+    });
