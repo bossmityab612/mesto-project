16b0d70f14a5513a40a09f25fe06cad1c5ac2490
Anastasia Bashkirova <bossmityab612@yandex.ru>
2022-08-31 18:25:04 +0300

    style: modif popup form

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const popupOpened = document.querySelector('.popup'); //Всплывающее окно
 const popup = document.querySelector('.popup__container');
 const openPopupButton = document.querySelectorAll('.profile__button-edit'); //Кнопка открытия попапа
-const closePopupButton = document.querySelector('.popup__container_button_exit'); //Кнопка закрытия попапа
+const closePopupButton = document.querySelector('.popup__button-exit'); //Кнопка закрытия попапа
 
 openPopupButton.forEach((button) => {
     button.addEventListener('click', (e) => {
