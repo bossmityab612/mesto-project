f6cd100daa90ff7ed8af90b9d848852b33603cf4
Anastasia Bashkirova <bossmityab612@yandex.ru>
2022-10-05 22:41:42 +0300

    style: popup-form edit

--- script/script.js
+++ script/script.js
@@ -1,22 +1,19 @@
-// const popupOpened = document.querySelector('.popup'); //Всплывающее окно
-// const popup = document.querySelector('.popup__container');
-// const openPopupButton = document.querySelectorAll('.profile__button-edit'); //Кнопка открытия попапа
-// const closePopupButton = document.querySelector('.popup__button-exit'); //Кнопка закрытия попапа
-const popup = document.querySelector('popup');
+const popupOpened = document.querySelector('.popup'); //Всплывающее окно
+const popup = document.querySelector('.popup__container');
+const openPopupButton = document.querySelectorAll('.profile__button-edit'); //Кнопка открытия попапа
+const closePopupButton = document.querySelector('.popup__button-exit'); //Кнопка закрытия попапа
 
-popup.classList.toggle('popup__opened', true);
+openPopupButton.forEach((button) => {
+        button.addEventListener('click', (popup) => {
+                popup.preventDefault();
+        popupOpened.classList.add('popup__opened');
+        popup.classList.add('popup__opened');
+    })
+});
 
-// openPopupButton.forEach((button) => {
-//     button.addEventListener('click', (popup) => {
-//         popup.preventDefault();
-//         popupOpened.classList.add('popup__opened');
-//         popup.classList.add('popup__opened');
-//     })
-// });
-
-// closePopupButton.addEventListener('click',() => {
-//         popupOpened.classList.remove('popup__opened');
-//         popup.classList.remove('popup__opened');
-//     });
+closePopupButton.addEventListener('click',() => {
+        popupOpened.classList.remove('popup__opened');
+        popup.classList.remove('popup__opened');
+    });
 
 
