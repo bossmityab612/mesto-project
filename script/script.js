4d4fb7b3ef0279590e14b8e79dd280c1ba8198f3
Anastasia Bashkirova <bossmityab612@yandex.ru>
2022-10-05 22:54:55 +0300

    js: form sendler popup

--- script/script.js
+++ script/script.js
@@ -3,6 +3,10 @@ const popup = document.querySelector('.popup__container');
 const openPopupButton = document.querySelectorAll('.profile__button-edit'); //Кнопка открытия попапа
 const closePopupButton = document.querySelector('.popup__button-exit'); //Кнопка закрытия попапа
 
+const popupForm = document.querySelector('.popup__form');
+const nameInput = document.querySelector('.popup__form_name');
+const jobInput = document.querySelector('.popup__form_job');
+
 openPopupButton.forEach((button) => {
         button.addEventListener('click', (popup) => {
                 popup.preventDefault();
@@ -14,6 +18,10 @@ openPopupButton.forEach((button) => {
 closePopupButton.addEventListener('click',() => {
         popupOpened.classList.remove('popup__opened');
         popup.classList.remove('popup__opened');
-    });
+});
 
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+}
 
+formElement.addEventListener('submit', formSubmitHandler);
