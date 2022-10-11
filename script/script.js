78e40c3c20a49d5d9e8d14ceb0e66eac2e587b94
Anastasia Bashkirova <bossmityab612@yandex.ru>
2022-10-12 02:04:18 +0300

    script: form on process

--- script/script.js
+++ script/script.js
@@ -1,27 +1,35 @@
-const popupOpened = document.querySelector('.popup'); //Всплывающее окно
-const popup = document.querySelector('.popup__container');
-const openPopupButton = document.querySelectorAll('.profile__button-edit'); //Кнопка открытия попапа
+const popup = document.querySelector('.popup'); //Всплывающее окно
+const openPopupButton = document.querySelector('.profile__button-edit'); //Кнопка открытия попапа
 const closePopupButton = document.querySelector('.popup__button-exit'); //Кнопка закрытия попапа
 
+const editButton = document.querySelector('.profile__button-edit')
 const popupForm = document.querySelector('.popup__form');
-const nameInput = document.querySelector('.popup-form-name');
-const jobInput = document.querySelector('.popup-form-job');
+const nameInput = document.querySelector('.popup-form_name');
+const jobInput = document.querySelector('.popup-form_job');
 
-openPopupButton.forEach((button) => {
-        button.addEventListener('click', (popup) => {
-                popup.preventDefault();
-        popupOpened.classList.add('popup__opened');
-        popup.classList.add('popup__opened');
-    })
-});
+openPopupButton.addEventListener('click', () => {
+        popup.classList.add('popup_opened');
+        popup.classList.add('popup_opened');
+})
 
 closePopupButton.addEventListener('click',() => {
-        popupOpened.classList.remove('popup__opened');
-        popup.classList.remove('popup__opened');
+        popup.classList.remove('popup_opened');
+        popup.classList.remove('popup_opened');
 });
 
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-}
+editButton.addEventListener('click', () => {
+        const userData = userInfo.getUserInfo();
+        nameInput.value = userData.name;
+        jobInput.value = userData.job;
+      
+        formValidators['profile'].resetValidation()
+      
+        profilePopup.open();
+      });
 
-popupForm.addEventListener('submit', formSubmitHandler);
+// function formSubmitHandler (evt) {
+//     evt.preventDefault();
+// }
+
+// popupForm.addEventListener('submit', formSubmitHandler);
+
