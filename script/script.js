2d6439d4d0746763e17c597cceebddb4e6379143
Anastasia Bashkirova <bossmityab612@yandex.ru>
2022-12-31 20:46:29 +0300

    script: function of likes

--- script/script.js
+++ script/script.js
@@ -1,12 +1,21 @@
 const popup = document.querySelector('.popup'); //Всплывающее окно
 const openPopupButton = document.querySelector('.profile__button-edit'); //Кнопка открытия попапа
 const closePopupButton = document.querySelector('.popup__button-exit'); //Кнопка закрытия попапа
+const like = document.querySelectorAll('.photo__like') // Переключашка для лайков
 
 const popupForm = document.querySelector('.popup__form'); //Поле формы
 const nameInput = document.querySelector('.profile__text-name'); //Поле имени
 const jobInput = document.querySelector('.profile__text-career'); //Поле профессии
 const nameForm = document.querySelector('.popup__form-name'); // Форма заполнения имени
 const jobForm = document.querySelector('.popup__form-job'); //Форма заполнения профессии
+
+// Функция для лайков
+
+like.forEach(function(el) {
+        el.addEventListener('click', function(evt) {
+                evt.target.classList.toggle('photo__like_active');
+        });
+});
 
 // Открытие и закрытие попапа
 
